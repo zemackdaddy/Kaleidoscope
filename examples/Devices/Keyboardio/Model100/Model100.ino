49399377db7036e760488ecf86769180fdea21bf
Maury Hill <maury.hill@metastock.com>
2024-01-16 19:15:58 -0600

    initial commit

--- examples/Devices/Keyboardio/Model100/Model100.ino
+++ examples/Devices/Keyboardio/Model100/Model100.ino
@@ -100,6 +100,15 @@
 // Support for the GeminiPR Stenography protocol
 #include "Kaleidoscope-Steno.h"
 
+#include <Kaleidoscope-TapDance.h>
+
+#include "Kaleidoscope-HostOS.h"
+#include "Kaleidoscope-Ranges.h"
+
+#include "AppSwitcher.h"
+
+#include <Kaleidoscope-AutoShift.h>
+
 /** This 'enum' is a list of all the macros used by the Model 100's firmware
   * The names aren't particularly important. What is important is that each
   * is unique.
@@ -187,8 +196,8 @@ enum {
   *
   */
 
-#define PRIMARY_KEYMAP_QWERTY
-// #define PRIMARY_KEYMAP_DVORAK
+//#define PRIMARY_KEYMAP_QWERTY
+#define PRIMARY_KEYMAP_DVORAK
 // #define PRIMARY_KEYMAP_COLEMAK
 // #define PRIMARY_KEYMAP_CUSTOM
 
@@ -202,14 +211,14 @@ KEYMAPS(
 
 #if defined (PRIMARY_KEYMAP_QWERTY)
   [PRIMARY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, LCTRL(Key_Spacebar),
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
+   AppSwitcher_Next, M(MACRO_ANY), Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
@@ -219,17 +228,17 @@ KEYMAPS(
 #elif defined (PRIMARY_KEYMAP_DVORAK)
 
   [PRIMARY] = KEYMAP_STACKED
-  (___,          Key_1,         Key_2,     Key_3,      Key_4, Key_5, Key_LEDEffectNext,
+  (___,          Key_1,         Key_2,     Key_3,      Key_4, Key_5, LCTRL(Key_Spacebar),
    Key_Backtick, Key_Quote,     Key_Comma, Key_Period, Key_P, Key_Y, Key_Tab,
    Key_PageUp,   Key_A,         Key_O,     Key_E,      Key_U, Key_I,
    Key_PageDown, Key_Semicolon, Key_Q,     Key_J,      Key_K, Key_X, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),   Key_6, Key_7, Key_8, Key_9, Key_0, LockLayer(NUMPAD),
+   AppSwitcher_Next,   Key_6, Key_7, Key_8, Key_9, Key_0, LockLayer(NUMPAD),
    Key_Enter,      Key_F, Key_G, Key_C, Key_R, Key_L, Key_Slash,
-                   Key_D, Key_H, Key_T, Key_N, Key_S, Key_Minus,
-   Key_RightAlt,   Key_B, Key_M, Key_W, Key_V, Key_Z, Key_Equals,
+		   Key_D, TD(0), Key_T, Key_N, Key_S, Key_Minus,
+   LGUI(LCTRL(Key_Slash)),   Key_B, Key_M, Key_W, Key_V, Key_Z, Key_Equals,
    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
@@ -292,16 +301,16 @@ KEYMAPS(
 
   [FUNCTION] =  KEYMAP_STACKED
   (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_CapsLock,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
+   Key_Tab,  LSHIFT(LCTRL(Key_Tab)),  Key_mouseUp, LCTRL(Key_Tab),   Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
    Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
    Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
    ___, Key_Delete, ___, ___,
    ___,
 
-   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
+   TD(1),   Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
+   Key_LeftArrow,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
                                Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
+   LALT(LCTRL(Key_LeftArrow)),          LALT(LCTRL(Key_RightArrow)),          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
    ___, ___, Key_Enter, ___,
    ___)
 ) // KEYMAPS(
@@ -457,6 +466,19 @@ USE_MAGIC_COMBOS({.action = toggleKeyboardProtocol,
                   // Left Fn + Prog + Shift
                   .keys = {R3C6, R0C0, R3C7}});
 
+void tapDanceAction(uint8_t tap_dance_index, KeyAddr key_addr, uint8_t tap_count,
+                    kaleidoscope::plugin::TapDance::ActionType tap_dance_action) {
+  switch (tap_dance_index) {
+  case 0:
+    return tapDanceActionKeys(tap_count, tap_dance_action,
+                              Key_H, Key_D);
+  case 1:
+    return tapDanceActionKeys(tap_count, tap_dance_action,
+                              LSHIFT(Key_9), LSHIFT(Key_0));
+  }
+}
+
+
 // First, tell Kaleidoscope which plugins you want to use.
 // The order can be important. For example, LED effects are
 // added in the order they're listed here.
@@ -611,7 +633,14 @@ KALEIDOSCOPE_INIT_PLUGINS(
 
   // The hardware test mode, which can be invoked by tapping Prog, LED and the
   // left Fn button at the same time.
-  HardwareTestMode  //,
+  HardwareTestMode,
+
+  AutoShift,
+  AutoShiftConfig,
+
+  TapDance,
+  HostOS,
+  AppSwitcher //,
 );
 
 /** The 'setup' function is one of the two standard Arduino sketch functions.
@@ -621,6 +650,8 @@ KALEIDOSCOPE_INIT_PLUGINS(
 void setup() {
   // First, call Kaleidoscope's internal setup function
   Kaleidoscope.setup();
+
+  HostOS.os(kaleidoscope::hostos::MACOS);
 
   // Set the hue of the boot greeting effect to something that will result in a
   // nice green color.
@@ -680,6 +711,23 @@ void setup() {
   // firmware starts with LED effects off. This avoids over-taxing devices that
   // don't have a lot of power to share with USB devices
   DefaultLEDModeConfig.activateLEDModeIfUnconfigured(&LEDOff);
+
+  // Enable AutoShift for letter keys and number keys only:
+  AutoShift.setEnabled(AutoShift.letterKeys() | AutoShift.numberKeys());
+  // Add symbol keys to the enabled categories:
+  AutoShift.enable(AutoShift.symbolKeys());
+  // Set the AutoShift long-press time to 150ms:
+  AutoShift.setTimeout(150);
+
+  QUKEYS(
+    kaleidoscope::plugin::Qukey(0, KeyAddr(2, 1), Key_LeftGui),      // A/cmd
+    kaleidoscope::plugin::Qukey(0, KeyAddr(2, 9), ShiftToLayer(2))   // Q/layer-shift (on `fn`)
+  )
+  Qukeys.setHoldTimeout(1000);
+  Qukeys.setOverlapThreshold(50);
+  Qukeys.setMinimumHoldTime(100);
+  Qukeys.setMinimumPriorInterval(80);
+  Qukeys.setMaxIntervalForTapRepeat(150);
 }
 
 /** loop is the second of the standard Arduino sketch functions.
@@ -688,7 +736,6 @@ void setup() {
   * For Kaleidoscope-based keyboard firmware, you usually just want to
   * call Kaleidoscope.loop(); and not do anything custom here.
   */
-
 void loop() {
   Kaleidoscope.loop();
 }
